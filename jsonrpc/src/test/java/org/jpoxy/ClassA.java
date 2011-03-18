2244bef9165ff9ac8e1136e310dff99c3edbc19a
kai5263499 <kai5263499@ebdeb907-6289-4657-dac1-05deb10069c1>
2011-03-18 01:20:40 +0000

    fixed unit tests

---
+++ jsonrpc/src/test/java/org/jpoxy/ClassA.java
@@ -0,0 +1,14 @@
+package org.jpoxy;
+
+public class ClassA {
+
+    public boolean init = false;
+
+    public ClassA() {
+        init = true;
+    }
+
+    public static String test() {
+        return "test from class A successful";
+    }
+}
---
+++ jsonrpc/src/test/java/org/jpoxy/Example.java
@@ -0,0 +1,50 @@
+package org.jpoxy;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.json.JSONObject;
+
+public class Example {
+
+    public boolean init = false;
+
+    public Example() {
+        init = true;
+    }
+
+    public static String test() {
+        return "test successful";
+    }
+
+    @SuppressWarnings("unused")
+    private String hidden() {
+        return "hidden method";
+    }
+
+    public float add(float a, float b) {
+        return a + b;
+    }
+
+    public float sub(float a, float b) {
+        return a - b;
+    }
+
+    public String list(int a, int b, int c) {
+        return "a:" + a + " b:" + b + " c:" + c;
+    }
+
+    public String echo(String s) {
+        return s;
+    }
+
+    public String echoJsonString(JSONObject obj)
+            throws UnsupportedEncodingException {
+        return URLEncoder.encode(obj.toString(), "UTF-8");
+    }
+
+    public JSONObject echoJsonObj(JSONObject obj)
+            throws UnsupportedEncodingException {
+        return obj;
+    }
+}
