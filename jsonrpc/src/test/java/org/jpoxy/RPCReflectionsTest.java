db2321fb26674628de9efc1d30c1a60b80036846
kai5263499 <kai5263499@ebdeb907-6289-4657-dac1-05deb10069c1>
2011-03-22 21:03:38 +0000

    initial test with reflections api

---
+++ jsonrpc/src/test/java/org/jpoxy/RPCReflectionsTest.java
@@ -0,0 +1,73 @@
+package org.jpoxy;
+
+import junit.framework.TestCase;
+import org.json.JSONArray;
+
+import org.json.JSONObject;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.testing.ServletTester;
+
+public class RPCReflectionsTest extends TestCase {
+
+    private static ServletTester tester;
+
+    @Override
+    protected void setUp() throws Exception {
+        tester = new ServletTester();
+        tester.setContextPath("/");
+        ServletHolder sh = tester.addServlet(org.jpoxy.RPC.class, "/api");
+        sh.setInitParameter("rpcclasses", "org.jpoxy.Class*");
+        sh.setInitParameter("expose_methods", "true");
+        sh.setInitParameter("detailed_errors", "true");
+        tester.start();
+    }
+
+    @Override
+    protected void tearDown() {
+        // release objects under test here, if necessary
+        tester = null;
+    }
+
+    private void checkHeader(String[] chunks) {
+        assertEquals(5, chunks.length);
+        assertEquals("HTTP/1.1 200 OK", chunks[0]);
+        assertEquals("Content-Type: text/plain; charset=iso-8859-1", chunks[1]);
+        // assertEquals("Content-Length: 877", chunks[2]);
+        assertTrue(chunks[2].matches("^Content-Length: \\d+$"));
+        assertEquals("", chunks[3]);
+    }
+
+    public void testListMethods() throws Exception {
+        String requests = "GET /api HTTP/1.1\r\n" + "Host: tester\r\n" + "\r\n";
+
+        String responses = tester.getResponses(requests);
+
+        System.out.println("responses: "+responses);
+
+        String chunks[] = responses.split("\\r\\n");
+
+        JSONObject jsonObj = new JSONObject(chunks[4]);
+
+        assertNotNull(jsonObj);
+        assertTrue(jsonObj.has("jsonrpc"));
+        assertEquals("2.0", jsonObj.getString("jsonrpc"));
+
+        JSONObject resultObj = jsonObj.getJSONObject("result");
+        assertNotNull(resultObj);
+
+        JSONArray methodArr = resultObj.getJSONArray("method");
+        assertNotNull(methodArr);
+        assertEquals(3, methodArr.length());
+
+        for (int i = 0; i < methodArr.length(); i++) {
+            JSONObject methodObj = methodArr.getJSONObject(i);
+            assertNotNull(methodObj);
+
+            assertTrue(methodObj.has("class"));
+            assertTrue(methodObj.has("name"));
+            assertTrue(methodObj.has("params"));
+            assertTrue(methodObj.has("returns"));
+            assertTrue(methodObj.has("static"));
+        }
+    }
+}
