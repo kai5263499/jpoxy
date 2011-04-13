5bf7eddd61b05632665bfadb3cd23c37b7c0d5af
kai5263499 <kai5263499@ebdeb907-6289-4657-dac1-05deb10069c1>
2011-04-13 18:58:40 +0000

    fixed init in RPC to run super init in order for methods like getServletContext to work, added context to init event message, fixed extjs filter test

--- trunk/jsonrpc/src/main/java/org/jpoxy/events/JSONRPCMessage.java
+++ trunk/jsonrpc/src/main/java/org/jpoxy/events/JSONRPCMessage.java
@@ -1,37 +1,45 @@
 package org.jpoxy.events;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 
 public class JSONRPCMessage {
-	/*
-	 * The following are message types which also serve to indicate the type
-	 * of information that is also being supplied in the data field.
-	 */
-	public static final int INIT          = 10;
-	public static final int EXCEPTION     = 60;
-	
-	private int code;
-	
-	private ServletConfig sevletconfig;
-	
-	public JSONRPCMessage( int code) {
-    	this.code = code;
+    /*
+     * The following are message types which also serve to indicate the type
+     * of information that is also being supplied in the data field.
+     */
+
+    public static final int INIT = 10;
+    public static final int EXCEPTION = 60;
+    private int code;
+    private ServletConfig sevletconfig;
+    private ServletContext servletcontext;
+
+    public JSONRPCMessage(int code) {
+        this.code = code;
     }
-    
-	public int getCode() {
-		return code;
-	}
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+    public int getCode() {
+        return code;
+    }
 
-	public void setServletConfig(ServletConfig serverconfig) {
-		this.sevletconfig = serverconfig;
-	}
+    public void setCode(int code) {
+        this.code = code;
+    }
 
-	public ServletConfig getServletConfig() {
-		return sevletconfig;
-	}
+    public void setServletConfig(ServletConfig serverconfig) {
+        this.sevletconfig = serverconfig;
+    }
 
+    public ServletConfig getServletConfig() {
+        return sevletconfig;
+    }
+
+    public void setServletContext(ServletContext servletcontext) {
+        this.servletcontext = servletcontext;
+    }
+
+    public ServletContext getServletcontext() {
+        return servletcontext;
+    }
 }
